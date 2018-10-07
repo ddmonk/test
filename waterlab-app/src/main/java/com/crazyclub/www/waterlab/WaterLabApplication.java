cd90d67ea4436243aaf9e376e0fc5449e7980268
gugang <ddmonk110@gmail.com>
2018-10-07 16:21:15 +0800

    ADD SOMETIME

--- waterlab-app/src/main/java/com/crazyclub/www/waterlab/WaterLabApplication.java
+++ waterlab-app/src/main/java/com/crazyclub/www/waterlab/WaterLabApplication.java
@@ -1,5 +1,11 @@
 package com.crazyclub.www.waterlab;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class WaterLabApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WaterLabApplication.class,args);
+    }
 }
